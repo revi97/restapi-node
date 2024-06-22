b17fb63d0774585ed7b1bae78484d648bb5a4a95
Revanth <revanthsrikantamurthy@gmail.com>
2024-06-23 00:19:51 +0530

    Added the post api to add users to file

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
 const PORT = 9000;
 
-app.get("/users", (req, res) => {
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/users", (req, res) => {
   return res.json(users);
+});
+
+app.route("/api/users/:id").get((req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.send(user);
+});
+
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ id: users.length + 1, ...body });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success", id: users.length });
+  });
 });
 
 app.listen(PORT, () => console.log(`Server is started at port ${PORT}`));
